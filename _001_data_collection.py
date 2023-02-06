9b7f3d6aacb5b67bb6d2f176c62358c4b37b7cd6
Karol Lukaszczyk <lukkarcontact@gmail.com>
2023-02-06 20:40:34 +0100

    update

--- _001_data_collection.py
+++ _001_data_collection.py
@@ -9,15 +9,16 @@ Additional parameters are:
 Arguments could be passed from the global config data file or directly into the function.
 """
 # Python
+import random
 from typing import Any, Union
 import time
 
 # External
 from selenium import webdriver
 from selenium.common.exceptions import (
-    NoSuchElementException, ElementClickInterceptedException, WebDriverException)
+    NoSuchElementException, ElementClickInterceptedException, WebDriverException, TimeoutException)
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support.wait import WebDriverWait
 import requests
 # import pandas as pd
 
@@ -39,15 +40,83 @@ def get_df_job_postings(
 
     url = get_url(config)
     driver = get_webpage(url, debug_mode)
-    jobs: JobsList = []
+    jobs_rows: JobsList = []
 
-    while len(jobs) < jobs_number:
+    while len(jobs_rows) < jobs_number:
 
         rid_off_pop_ups(driver)
 
         # Going through each job in this page
         # jl for Job Listing. These are the buttons we're going to click.
-        job_buttons = driver.find_elements(By.CSS_SELECTOR, "jl")
+
+        jobs_list_buttons = await_element(
+            driver, 10, By.XPATH, '//ul[@data-test="jlGrid"]')
+
+        # jobs_list_buttons = driver.find_element(
+        #     By.XPATH, '//ul[@data-test="jlGrid"]')
+
+        jobs_buttons = jobs_list_buttons.find_elements(
+            By.TAG_NAME, "li")
+
+        counter = 0
+        for job_button in jobs_buttons:
+
+            rand_sleep = random.uniform(0.5, 1.4)
+
+            print("Progress: {}".format(
+                "" + str(len(jobs_rows) + counter) + "/" + str(jobs_number)))
+            if len(jobs_rows) >= jobs_number:
+                break
+
+            print("not clicked", counter)
+
+            job_button.click()  # You might
+            print("clicked", counter)
+
+            counter += 1
+            time.sleep(rand_sleep)
+            collected_successfully = False
+
+            job_description = await_element(
+                driver, 5, By.ID, 'JDCol')
+
+            click_x_pop_up(driver)
+
+            while not collected_successfully:
+                try:
+                    company_name = job_description.find_element(
+                        By.XPATH, './/div[@data-test="employerName"]').text
+                    location = job_description.find_element(
+                        By.XPATH, './/div[@data-test="location"]').text
+                    job_title = job_description.find_element(
+                        By.XPATH, './/div[@data-test="jobTitle"]').text
+                    description = job_description.find_element(
+                        By.XPATH, './/div[@class="jobDescriptionContent desc"]').text
+                    try:
+                        rating_overall = job_description.find_element(
+                            By.XPATH, './/div[@data-test="rating-info"]').text
+
+                        rating_overall = rating_overall[0:3]
+
+                    except NoSuchElementException:
+                        rating_overall = -1
+
+                    print(rating_overall)
+                    collected_successfully = True
+                    # location, job_title, job_description)
+                except:
+                    time.sleep(5)
+
+            try:
+                salary_estimate = job_button.find_element(
+                    By.XPATH, './/span[@data-test="detailSalary"]').text
+            except NoSuchElementException:
+                salary_estimate = -1  # You need to set a "not found value. It's important."
+
+
+def await_element(driver, time, by, elem):
+    return WebDriverWait(driver, time).until(
+        lambda x: x.find_element(by, elem))
 
 
 def rid_off_pop_ups(driver):
@@ -65,9 +134,11 @@ def click_x_pop_up(driver):
     """pass pop-up"""
 
     try:
-        driver.find_element(By.CSS_SELECTOR, '[alt="Close"]').click()
+        x_button = await_element(
+            driver, 3, By.CSS_SELECTOR, '[alt="Close"]')
+        x_button.click()
         print('x out worked')
-    except NoSuchElementException:
+    except (NoSuchElementException, TimeoutException):
         print('x out failed')
 
 
@@ -110,7 +181,7 @@ def get_driver(
     # Change the path to where chromedriver/other browser is if you need to.
     driver = webdriver.Chrome(
         executable_path=path, options=options)
-    driver.set_window_rect(width=1120, height=1000)
+    # driver.set_window_rect(width=1120, height=1000)
     return driver
 
 
