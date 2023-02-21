672db2ac034c579b63284734b3691828dc30fca9
Karol Lukaszczyk <lukkarcontact@gmail.com>
2023-02-21 11:04:50 +0100

    terminal separator

--- scraper/helpers/print_key_value_pairs.py
+++ scraper/helpers/print_key_value_pairs.py
@@ -3,6 +3,8 @@ This module provides a function
 that prints key-value pairs for a Job object,
 which is used for debugging when parsing HTML.
 '''
+# Python
+from os import get_terminal_size
 
 # Internal
 from scraper._types import Job
@@ -14,4 +16,6 @@ def print_key_value_pairs(job: Job):
     for index, (key, value) in enumerate(job.items()):
         print(f"{index + 1}. {key}:\n{value}")
 
-    print("\n@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n")
+    terminal_width = get_terminal_size().columns
+    separator = f"\n{'=' * terminal_width}\n"
+    print(separator)
