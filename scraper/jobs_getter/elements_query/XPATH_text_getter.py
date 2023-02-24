e1cce5222c79cd859c80a681480dfd839d72c4d5
Karol Lukaszczyk <lukkarcontact@gmail.com>
2023-02-24 18:14:28 +0100

    XPATH def docstrings

--- scraper/jobs_getter/elements_query/XPATH_text_getter.py
+++ scraper/jobs_getter/elements_query/XPATH_text_getter.py
@@ -1,9 +1,9 @@
-
-'''
+"""
 This module provides a function that returns the text of a selected element
 or elements using an XPath query on a web page, with the option to return a list of texts and
 a fallback "NA_value" from the configuration file.
-'''
+"""
+
 # External
 from selenium.webdriver.common.by import By
 
@@ -18,16 +18,34 @@ Elements = list[MyWebElement]
 
 class XpathSearch:
     def __init__(self, element: str):
+        """
+        A class representing an XPath search.
+
+        Args:
+        - element (str): an XPath query string for the desired element.
+        """
         self.value = na_value
         self.element = element
 
 
 class XpathListSearch(XpathSearch):
+    """
+    A class representing an XPath search that returns a list of elements.
+    """
     pass
 
 
 def get_XPATH_values(source_html: MyWebElement, search: XpathSearch | XpathListSearch) -> list | str:
-    '''return text or texts of selected web element'''
+    """
+    Extracts the text or texts of selected web element.
+
+    Args:
+    - source_html (MyWebElement): the web page element to search.
+    - search (XpathSearch or XpathListSearch): a class representing an XPath query for the desired element(s).
+
+    Returns:
+    - str or list[str]: the text or texts of the selected web element(s).
+    """
 
     if isinstance(search, XpathListSearch):
 
@@ -46,15 +64,33 @@ def get_XPATH_values(source_html: MyWebElement, search: XpathSearch | XpathListS
         return text
 
 
-def _get_all_texts(elements):
+def _get_all_texts(elements: Elements) -> list[str]:
+    """
+    Extracts the text of a list of web elements.
 
+    Args:
+    - elements: a list of web elements.
+
+    Returns:
+    - list[str]: a list of the text of the input web elements.
+    """
     texts: list[str] = []
     for elem in elements:
         texts.append(elem.text)
     return texts
 
 
-def _get_text(source_html, search):
+def _get_text(source_html: MyWebElement, search: XpathSearch) -> str:
+    """
+    Extracts the text of a single web element.
+
+    Args:
+    - source_html (MyWebElement): the web page element to search.
+    - search (XpathSearch): a class representing an XPath query for the desired element.
+
+    Returns:
+    - str: the text of the selected web element.
+    """
     return source_html.find_element(
         By.XPATH, search.element
     ).text
