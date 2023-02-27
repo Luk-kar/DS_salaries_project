b74bc9052dc4bdd8ea2884432aab989df1b91806
Karol Lukaszczyk <lukkarcontact@gmail.com>
2023-02-27 17:10:57 +0100

    update writer with comments

--- scraper/jobs_to_csv/CSV_Writer.py
+++ scraper/jobs_to_csv/CSV_Writer.py
@@ -1,3 +1,6 @@
+'''
+This module defines classes to handle writing to CSV files.
+'''
 # Python
 import csv
 import os
@@ -14,14 +17,53 @@ Mode = Literal["w", "a"]
 
 
 class CSV_Writer():
+    '''
+    This class writes job posting to CSV files. 
+
+    Attributes:
+    - csv_path (str): Path to the CSV file.
+
+    - directory_path (str): Path to the directory containing the CSV file.
+
+    - encoding (str): The encoding of the CSV file.
+
+    - counter (int): A counter used to keep track of the number of rows written.
+
+    Methods:
+    - write_observation(observation: Job): Write a row of job observation data to the CSV file.
+
+    - write_row(row: Job): Writes a row of job observation data to the CSV file.
+
+    - write_header(header: Job): Writes the header row to the CSV file.
+
+    - _my_write_row(row: tuple | Job, file_path: str, mode: Mode, encoding: str): 
+    A private method that writes a row to the CSV file.
+
+    - _print_write_error(file_path: str, error: csv.Error): 
+    A private method that prints an error message to the console 
+    if there is an error writing to the CSV file.
+
+    - _convert_dict_values_to_tuple(dictionary: dict) -> tuple: 
+    A private method that converts a dictionary of job observation data to a tuple.
+
+    '''
 
     def __init__(self, csv_path: str) -> None:
+
         self.csv_path = csv_path
         self.directory_path = os.path.dirname(csv_path)
         self.encoding = get_encoding()
         self.counter = 1
 
     def write_observation(self, observation: Job):
+        '''
+        Write a row of job observation data to the CSV file.
+
+        Args:
+        - observation (Job): A dictionary containing the job observation data 
+        to write to the CSV file.
+
+        '''
 
         if self.counter == 1:
             self.write_header(observation)
@@ -32,9 +74,13 @@ class CSV_Writer():
 
     def write_row(self, row: Job):
         '''
-        appends list of tuples in specified output csv file
-        a tuple is written as a single row in csv file
+        Writes a row of job observation data to the CSV file.
+
+        Args:
+        - row (Job): A dictionary containing the job observation data to write to the CSV file.
+
         '''
+
         file_path = self.csv_path
         encoding = self.encoding
 
@@ -49,6 +95,13 @@ class CSV_Writer():
         self._my_write_row(row_tupled, file_path, "a", encoding)
 
     def write_header(self, header: Job):
+        '''
+        Writes the header row to the CSV file.
+
+        Args:
+        - header (Job): A dictionary containing the header data to write to the CSV file.
+
+        '''
 
         file_path = self.csv_path
         encoding = self.encoding
@@ -59,6 +112,17 @@ class CSV_Writer():
         self._my_write_row(header, file_path, "w", encoding)
 
     def _my_write_row(self, row: tuple | Job, file_path: str, mode: Mode, encoding: str):
+        '''
+        A private method that writes a row to the CSV file.
+
+        Args:
+        - row (tuple | Job): A tuple or dictionary containing the job observation data
+          to write to the CSV file.
+        - file_path (str): Path to the CSV file.
+        - mode (Mode): Write mode ('w' for overwrite or 'a' for append).
+        - encoding (str): The encoding of the CSV file.
+
+        '''
 
         with open(file_path, mode, newline='', encoding=encoding) as csv_file:
 
@@ -71,6 +135,14 @@ class CSV_Writer():
                 self._print_write_error(file_path, error)
 
     def _print_write_error(self, file_path: str, error: csv.Error):
+        '''
+        A private method that prints an error message to the console 
+        if there is an error writing to the CSV file.
+
+        Args:
+        - file_path (str): Path to the CSV file.
+        - error (csv.Error): The error that occurred while writing to the CSV file.
+        '''
 
         line_number = self.counter + 1  # + 1 (header)
 
@@ -84,10 +156,30 @@ class CSV_Writer():
         )
 
     def _convert_dict_values_to_tuple(self, dictionary: dict) -> tuple:
+        '''
+        A private method that converts a dictionary of job observation data to a tuple.
+
+        Args:
+        - dictionary (dict): A dictionary containing job observation data.
+
+        Returns:
+        - tuple: A tuple containing the values of the input dictionary.
+        '''
         return tuple(dictionary.values())
 
 
 class CSV_Writer_RAW(CSV_Writer):
+    '''
+    This class writes job information to the raw CSV file.
+
+    Attributes:
+    - None
+
+    Methods:
+    - __init__(): Constructs the CSV_Writer_RAW instance by calling 
+    the parent class constructor with the path to the raw CSV file.
+
+    '''
 
     def __init__(self) -> None:
         super().__init__(
