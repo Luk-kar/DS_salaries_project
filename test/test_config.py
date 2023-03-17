1678bbddfde98a1a42378e2c275f6b231a283b7a
Karol Lukaszczyk <lukkarcontact@gmail.com>
2023-03-17 18:19:59 +0100

    tests update

---
+++ test/test_config.py
@@ -0,0 +1,161 @@
+'''
+This is a module for testing the configuration data of a web scraping project.
+The module consists of two test classes: TestConfigData and TestJobDescription.
+'''
+
+# Python
+import os
+import re
+import unittest
+
+# External
+from pathvalidate import sanitize_filepath, sanitize_filename
+import requests
+
+# Internal
+from scraper.config.get import get_config, get_url, get_path_csv_raw, get_path_csv_clean, get_NA_value, get_encoding
+from scraper.config._types import Config, JobNumber, JobSimilar, Url
+
+
+class TestConfigData(unittest.TestCase):
+    '''
+    It tests the configuration data stored in Config
+    object which is obtained from get_args method of scraper.config.get_config
+    '''
+
+    def setUp(self):
+        '''init all config values'''
+        self.config: Config = get_config()
+        self.url = get_url(self.config['url'],
+                           self.config['jobs_titles']['default'])
+
+    def _is_empty_string(self, string: str):
+        '''assert if is it not an empty string'''
+
+        self.assertIsInstance(string, str)
+        self.assertNotEqual(string, "")
+
+    def _is_valid_file_path(self, file_path: str) -> bool:
+        '''
+        Checks if the file path is valid and does not contain any illegal characters.
+        '''
+
+        # https://stackoverflow.com/a/67119769/12490791
+        is_valid = file_path == sanitize_filepath(
+            file_path, platform="auto")
+
+        return is_valid
+
+    def _is_valid_file_name(self, file_name: str) -> bool:
+        '''
+        Checks if the file path is valid and does not contain any illegal characters.
+        '''
+        is_valid = file_name == sanitize_filename(
+            file_name, platform="universal")
+
+        return is_valid
+
+    def test_config_is_dict(self):
+        '''assert if instance of a dict'''
+
+        self.assertIsInstance(self.config, dict)
+
+    def test_job_default(self):
+        '''assert if is it not an empty string'''
+
+        job_title = self.config['jobs_titles']['default']
+
+        self._is_empty_string(job_title)
+        self.assertTrue(
+            self._is_valid_file_name(job_title)
+        )
+
+    def test_jobs_similar(self):
+        '''assert if is any of the jobs, not an empty string'''
+
+        jobs: JobSimilar = self.config['jobs_titles']['similar']
+
+        self.assertIsInstance(jobs, list)
+
+        for job in jobs:
+            self._is_empty_string(job)
+            self.assertTrue(
+                self._is_valid_file_name(job)
+            )
+
+    def test_jobs_number(self):
+        '''test if is it greater than 0'''
+
+        jobs_number: JobNumber = self.config['jobs_number']
+        self.assertIsInstance(jobs_number, int)
+        self.assertGreater(jobs_number, 0)
+
+    def test_url(self):
+        '''test if are there url's types'''
+
+        url: Url = self.config['url']
+        self.assertIsInstance(url, dict)
+
+        for part in url:
+            self.assertIsInstance(part, str)
+
+    def test_is_url_exists(self):
+        '''check if url exists'''
+
+        response = requests.get(self.url, timeout=10)
+
+        NOT_EXISTS_code = 404
+        status_code = response.status_code
+
+        # For some reasons you get 403: Forbidden. But the whole app works anyway.
+        self.assertNotEqual(status_code, NOT_EXISTS_code,
+                            f"Not Found\nError - {status_code} : {response.reason}"
+                            )
+
+    def test_driver_path(self):
+        '''check if the driver exists on the local machine'''
+
+        path_file: str = self.config['driver_path']
+        self._is_empty_string(path_file)
+        self.assertTrue(os.path.exists(path_file))
+        self.assertTrue(os.path.isfile(path_file))
+
+        is_Mac_Win_Linux_app = \
+            path_file.endswith(".exe") or \
+            re.search(".*[^a]$", path_file)  # no extension: Linux, Mac
+
+        self.assertTrue(is_Mac_Win_Linux_app)
+
+    def test_debug_mode(self):
+        '''check an arg for verbose mode'''
+
+        self.assertIsInstance(self.config['debug_mode'], bool)
+
+    def test_NA_value(self):
+        '''check if NA is got'''
+
+        self.config['NA_value']
+        self.assertIsNotNone(get_NA_value())
+
+    def test_encoding_value(self):
+        """check if not an empty string"""
+
+        self._is_empty_string(get_encoding())
+
+    def test_output_path_raw(self):
+        '''check correctness of path'''
+
+        csv_raw_path = get_path_csv_raw()
+
+        self.assertTrue(self._is_valid_file_path(csv_raw_path))
+
+    def test_output_path_clean(self):
+        '''check correctness of path'''
+
+        csv_clean_path = get_path_csv_clean()
+
+        self.assertTrue(self._is_valid_file_path(csv_clean_path))
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ test/test_integration.py
@@ -0,0 +1,198 @@
+'''
+This is a module for testing the configuration data of a web scraping project.
+The module consists of two test classes: TestConfigData and TestJobDescription.
+'''
+
+# Python
+import os
+import csv
+import _csv
+import re
+import unittest
+from time import sleep
+import random
+
+# Internal
+from scraper.scraper import scrape_data
+from scraper.config.get import get_path_csv_raw, get_encoding
+
+
+class TestIntegration(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        '''init all config values'''
+
+        regex = {
+            # https://regex101.com/r/9m7gaB/1
+            'Non-empty-string': r"^(?!\s*$).+",
+            # https://regex101.com/r/XRho9L/1
+            '0.0-5.0': r"^(?:[0-4](?:\.[0-9])?|5(?:\.0)?|)$",
+            # https://regex101.com/r/vLx7JS/1
+            '24h-30d+': r"^(?:24h|1 day ago|[1-2]?[0-9]d?|30d\+?)$",
+            # https://regex101.com/r/9bIQDf/1
+            'bool': r"^(True|False)$",
+            # https://regex101.com/r/8TbXbC/1
+            'employees': r"^(?!.*employees).*$",
+            # https://regex101.com/r/g076Ht/1
+            'exists': r"^.*$",
+            # https://regex101.com/r/j32tP1/1
+            '1-9999': r"^(?:[1-9]\d{0,3}|)$",
+            # https://regex101.com/r/jVaJDa/1
+            'Revenue_USD': r"^(?!.*\(USD\)).*$",
+            # https://regex101.com/r/phTDbC/1
+            '0.00-5.00': r"^(?:0(\.[0-9]{1,2})?|([1-4](\.[0-9]{1,2})?)|5(\.0{1,2})?|)$",
+            # https://regex101.com/r/bGQCzp/1
+            'reviews': r"^(\[((\'|\").*(\'|\")(, )?)+\]|)$"
+        }
+
+        cls.jobs_number = 3
+        cls.csv = {
+            'path': "data\RAW\Data_Engineer_06-03-2023_23-41.csv",
+            'delimiter': ",",
+            'encoding': get_encoding(),
+            'expected_values': {
+                'Company_name': regex["Non-empty-string"],
+                'Rating': regex['0.0-5.0'],
+                'Location': regex['Non-empty-string'],
+                'Job_title': regex['Non-empty-string'],
+                'Description': regex['Non-empty-string'],
+                'Job_age': regex['24h-30d+'],
+                'Easy_apply': regex['bool'],
+                'Employees': regex['employees'],
+                'Type_of_ownership': regex['exists'],
+                'Sector': regex['exists'],
+                'Founded': regex['1-9999'],
+                'Industry': regex['exists'],
+                'Revenue_USD': regex['Revenue_USD'],
+                'Friend_recommend': regex['0.00-5.00'],
+                'CEO_approval': regex['0.00-5.00'],
+                'Career_opportunities': regex['0.0-5.0'],
+                'Comp_&_benefits': regex['0.0-5.0'],
+                'Culture_&_values': regex['0.0-5.0'],
+                'Senior_management': regex['0.0-5.0'],
+                'Work/Life_balance': regex['0.0-5.0'],
+                'Pros': regex['reviews'],
+                'Cons': regex['reviews'],
+                'Benefits_rating': regex['0.0-5.0'],
+            }
+        }
+        cls.target_folder = os.path.dirname(get_path_csv_raw())
+        cls.target_directory_files_before = cls._get_csv_files(
+            cls.target_folder)
+
+    def setUp(self) -> None:
+
+        # to avoid blockage from the glassdoor.com
+        self._wait_for_glassdoor_server()
+
+        return super().tearDown()
+
+    def test_in_debug_mode(self):
+
+        self._check_if_created_file_is_valid(
+            lambda: scrape_data(jobs_number=self.jobs_number, debug_mode=True)
+        )
+
+    def test_in_production(self):
+
+        self._check_if_created_file_is_valid(
+            lambda: scrape_data(jobs_number=self.jobs_number, debug_mode=False)
+        )
+
+    @staticmethod
+    def _wait_for_glassdoor_server():
+        time_span = random.uniform(1.25, 1.7)
+        sleep(time_span)
+
+    def _check_if_created_file_is_valid(self, scrape_data_function):
+
+        before_files = self._get_csv_files(
+            self.target_folder)
+
+        with self.assertRaises(SystemExit):
+            scrape_data_function()
+
+        after_files = self._get_csv_files(
+            self.target_folder)
+
+        difference = set(after_files) - set(before_files)
+
+        for filename in difference:
+            if filename not in before_files:
+                file_path = os.path.join(self.target_folder, filename)
+                self._test_csv_file_structure(file_path)
+                break
+
+    @staticmethod
+    def _get_csv_files(directory: str) -> list[str]:
+
+        csv_files = []
+
+        for filename in os.listdir(directory):
+            if filename.endswith('.csv'):
+                csv_files.append(filename)
+
+        return csv_files
+
+    def _test_csv_file_structure(self, csv_file_path: str):
+
+        delimiter = self.csv['delimiter']
+        expected_values: dict[str, str] = self.csv['expected_values']
+
+        with open(csv_file_path, newline="", encoding=self.csv['encoding']) as file:
+
+            reader = csv.reader(file, delimiter=delimiter)
+            headers = next(reader)
+
+            self._test_each_column(expected_values, reader, headers)
+
+    def _test_each_column(self, expected_values: dict[str, str], reader: _csv.reader, headers: list[str]):
+
+        for i, row in enumerate(reader):
+
+            self._test_each_field(expected_values, headers, i, row)
+
+    def _test_each_field(self, expected_values: dict[str, str], headers: list[str], i: int, row: list[str]):
+
+        for j, field in enumerate(row):
+            header = headers[j]
+            expected_regex = expected_values.get(header, None)
+
+            if expected_regex is not None:
+                assert re.match(
+                    expected_regex, field), f"Invalid value in row {i+2}, column {j+1}:\
+                        \nHeader :{header}:\nField  :{field}:\nExpect :{expected_regex}:"
+
+    def tearDown(self) -> None:
+
+        # to avoid blockage from the glassdoor.com
+        self._wait_for_glassdoor_server()
+
+        return super().tearDown()
+
+    @classmethod
+    def tearDownClass(cls):
+
+        target_folder = cls.target_folder
+        before_files = cls.target_directory_files_before
+
+        new_files = cls._get_created_files()
+
+        for filename in new_files:
+            if filename not in before_files:
+                new_file_path = os.path.join(target_folder, filename)
+                os.remove(new_file_path)
+
+        super().tearDownClass()
+
+    @classmethod
+    def _get_created_files(cls):
+
+        target_folder = cls.target_folder
+        before_files = cls.target_directory_files_before
+        after_files = cls._get_csv_files(target_folder)
+
+        difference = set(after_files) - set(before_files)
+
+        return difference
