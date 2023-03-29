1abfd63b8bc4bc63f3113d0bd942a8d021bf9335
Karol Lukaszczyk <lukkarcontact@gmail.com>
2023-03-29 14:53:19 +0200

    init

--- scraper/config/_types.py
+++ scraper/config/_types.py
@@ -7,19 +7,20 @@ from annotated_types import Gt
 
 JobDefault = str
 JobSimilar = list[str]
-JobTitles = TypedDict('JobTitles', {'default': JobDefault,
-                                    'similar': JobSimilar})
+JobTitles = TypedDict("JobTitles", {"default": JobDefault,
+                                    "similar": JobSimilar})
 JobNumber = Annotated[int, Gt(0)]
+Countries = list[str]
 Url = dict[str, str]
 DriverPath = str
 DebugMode = bool
 NA_value = Literal[""]
 Encoding = str
-OutputPath = TypedDict('OutputPath', {'main': str, 'raw': str, 'clean': str})
-Config = TypedDict('Config', {'jobs_titles': JobTitles, 'jobs_number': JobNumber,
-                              'url': Url, 'driver_path': DriverPath,
-                              'debug_mode': DebugMode, 'NA_value': NA_value,
-                              'output_path': OutputPath,
-                              'encoding': Encoding
+OutputPath = TypedDict("OutputPath", {"main": str, "raw": str, "clean": str})
+Config = TypedDict("Config", {"jobs_titles": JobTitles, "jobs_number": JobNumber,
+                              "countries": Countries, "url": Url, "driver_path": DriverPath,
+                              "debug_mode": DebugMode, "NA_value": NA_value,
+                              "output_path": OutputPath,
+                              "encoding": Encoding
                               },
                    )
