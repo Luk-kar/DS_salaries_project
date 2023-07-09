75f034ef89b5485e6723fd6ad1fe05565b862c4b
Karol Lukaszczyk <lukkarcontact@gmail.com>
2023-07-09 14:03:28 +0200

    update

--- FlaskAPI/http_request.py
+++ FlaskAPI/http_request.py
@@ -3,7 +3,7 @@ from data_input import data_in
 
 url = "http://127.0.0.1:5000/predict"
 headers = {"Content-Type": "application/json"}
-data = {"input": data_in["invalid"]}
+data = {"input": data_in["valid"]}
 
 response = requests.get(url, headers=headers, json=data)
 print(response.status_code)
